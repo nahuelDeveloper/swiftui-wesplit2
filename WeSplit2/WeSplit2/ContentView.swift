b7a31fc66a203cc77ef8c7e2c4cf8b4ce99c9596
Nahuel Jose Roldan <>
2024-03-16 23:03:43 -0300

    Add form and a section for the check amount.

--- WeSplit2/WeSplit2/ContentView.swift
+++ WeSplit2/WeSplit2/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+        
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    let currencyCode = Locale.current.currency?.identifier ?? "USD"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: currencyCode))
+                .keyboardType(.decimalPad)
+            }
+            Section {
+                    Text(checkAmount, format: .currency(code: currencyCode))
+                }
         }
-        .padding()
     }
 }
 
