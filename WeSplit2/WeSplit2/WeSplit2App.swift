409661cbdea80b05f35b1c720bc6b67ddc1dc855
Nahuel Jose Roldan <>
2024-03-16 22:39:46 -0300

    Initial commit.

---
+++ WeSplit2/WeSplit2/WeSplit2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit2App.swift
+//  WeSplit2
+//
+//  Created by Nahuel Jose Roldan on 16/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
